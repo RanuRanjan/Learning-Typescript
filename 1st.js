c218f30dc71039b30cf6859c5adb83753e179117
RanuRanjan <ranuranjan25@gmail.com>
2022-02-23 18:31:33 +0530

    hello world

---
+++ 1st.js
@@ -0,0 +1,12 @@
+var start = "hELLO WORLD";
+console.warn(start);
+var App = /** @class */ (function () {
+    function App() {
+    }
+    App.prototype.test = function () {
+        console.warn("first class");
+    };
+    return App;
+}());
+var obj = new App();
+obj.test();
---
+++ 1st.ts
@@ -0,0 +1,18 @@
+
+// var decalre
+let start="hELLO WORLD"
+
+// console
+console.warn(start);
+
+
+
+
+class App{
+    test(){
+        console.warn("first class")
+    }
+}
+
+let obj=new App()
+obj.test()

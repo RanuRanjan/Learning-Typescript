907fd9ce9282dffbe4c27d0430c5c24a055f82c0
RanuRanjan <ranuranjan25@gmail.com>
2022-02-24 22:40:04 +0530

    function

---
+++ function.js
@@ -0,0 +1,11 @@
+function add(x, y) {
+    return x + y;
+}
+var res = add(5, 4);
+console.log(res);
+function greater(a) {
+    if (a > 8) {
+        return true;
+    }
+}
+console.log(greater(res));
---
+++ function.ts
@@ -0,0 +1,15 @@
+function add(x:number,y:number){
+    return x+y
+}
+
+let res= add(5,4)
+console.log(res)
+
+
+function greater(a:number){
+    if(a>8){
+        return true
+    }
+}
+
+console.log(greater(res))
